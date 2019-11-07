4a0844fcdb4bedef0795ef73c510c545840dab57
Pavel Rozhkov <prozskov@gmail.com>
2019-11-07 13:41:27 +0700

    Update initialize file for usage with async FHIRClient

--- initialize.py
+++ initialize.py
@@ -4,17 +4,16 @@ import os
 import os.path
 
 
-def import_bundle(client, filename):
+async def import_bundle(client, filename):
     with open(filename) as fd:
         patient_json = json.load(fd)
     bundle = client.resource('Bundle')
     bundle['type'] = 'transaction'
-    bundle._get_path = lambda: ''
     bundle['entry'] = patient_json['entry']
-    bundle.save()
+    await bundle.save()
 
 
-def import_dataset(client, dataset_path):
+async def import_dataset(client, dataset_path):
     sys.stdout.write("Import progress: 0%   \r")
     
     filenames = [
@@ -23,10 +22,9 @@ def import_dataset(client, dataset_path):
 
     total_count = len(filenames)
     for index, filename in enumerate(filenames):
-        import_bundle(client, os.path.join(dataset_path, filename))
+        await import_bundle(client, os.path.join(dataset_path, filename))
         progress = int(float(index + 1) / float(total_count) * 100)
         sys.stdout.write("Import progress: %d%%   \r" % progress)
         sys.stdout.flush()
     sys.stdout.write("Import progress: 100%\n")
     sys.stdout.write("{0} bundles imported".format(total_count))
-
